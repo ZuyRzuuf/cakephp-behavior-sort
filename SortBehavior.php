83bdc37d533fd322dfc36823c91530be46f9eadd
Xathloc <krzysiek@sobieraj.mobi>
2013-10-18 14:32:24 +0200

    możliwość zmiany domyślnego pola do zapisu pozycji

--- SortBehavior.php
+++ SortBehavior.php
@@ -9,67 +9,74 @@
  */
 
 class SortBehavior extends ModelBehavior {
-    public function arrange(Model $model, $data = array())
-    {
-        if(empty($data)) return false;
-        
-        if(array_key_exists('list', $data)) {
+    public function setup(Model $model, $settings = array()) {
+        if (!isset($this->settings[$model->alias])) {
+            $this->settings[$model->alias] = array(
+                'position_field' => 'position',
+            );
+        }
+        $this->settings[$model->alias] = array_merge(
+                $this->settings[$model->alias], (array) $settings);
+    }
+
+    public function arrange(Model $model, $data = array()) {
+        if (empty($data))
+            return false;
+
+        if (array_key_exists('list', $data)) {
             for ($i = 0; $i < count($data['list']); $i++) {
                 $model->id = $data['list'][$i];
-                $model->saveField('position', ($i+1));
+                $model->saveField($this->settings[$model->alias]['position_field'], ($i + 1));
             }
         }
     }
-	
-    public function resort(Model $model)
-    {
+
+    public function resort(Model $model) {
         $objects = $model->find('all', array('fields' => array('id'),
-                                'order' => array('position ASC'),));
+            'order' => array('position ASC'),));
         $counter = 1;
         foreach ($objects as $object) {
             $model->id = $object[$model->alias]['id'];
-            $model->saveField('position', $counter);
+            $model->saveField($this->settings[$model->alias]['position_field'], $counter);
             $counter++;
         }
     }
-    
-    public function sort(Model $model, $direction, $position)
-    {
+
+    public function sort(Model $model, $direction, $position) {
         $oTarget = $model->findByPosition($position);
         $nCount = $model->find('count');
 
-        if(isset($direction)) {
-            if($direction == 'up' && $position > 1) {
+        if (isset($direction)) {
+            if ($direction == 'up' && $position > 1) {
                 $prev = $position - 1;
                 $oNeighbour = $model->findByPosition($prev);
                 $model->id = $oTarget[$model->alias]['id'];
-                $model->saveField('position', $prev);
+//                $model->saveField('position', $prev);
+                $model->saveField($this->settings[$model->alias]['position_field'], $prev);
                 $model->id = $oNeighbour[$model->alias]['id'];
-                $model->saveField('position', $oTarget[$model->alias]['position']);
-            }
-            else if($direction == 'down' && $position < $nCount) {
+                $model->saveField($this->settings[$model->alias]['position_field'], $oTarget[$model->alias]['position']);
+            } else if ($direction == 'down' && $position < $nCount) {
                 $next = $position + 1;
                 $oNeighbour = $model->findByPosition($next);
                 $model->id = $oTarget[$model->alias]['id'];
-                $model->saveField('position', $next);
+                $model->saveField($this->settings[$model->alias]['position_field'], $next);
                 $model->id = $oNeighbour[$model->alias]['id'];
-                $model->saveField('position', $oTarget[$model->alias]['position']);
+                $model->saveField($this->settings[$model->alias]['position_field'], $oTarget[$model->alias]['position']);
             }
-          }
-    
+        }
     }
-    
-    public function setLast(Model $model, $id)
-    {
-        $data = $model->find('first', array('fields' => array('position'),
-                             'order' => array('position DESC')));
+
+    public function setLast(Model $model, $id) {
+        $data = $model->find('first', array('fields' => array($this->settings[$model->alias]['position_field']),
+            'order' => array('position DESC')));
         debug($data);
-        if($data === null)
+        if ($data === null)
             $position = 1;
         else
             $position = $data[$model->alias]['position'] + 1;
-        
+
         $model->id = $id;
-        $model->saveField('position', $position);
+        $model->saveField($this->settings[$model->alias]['position_field'], $position);
     }
+
 }
